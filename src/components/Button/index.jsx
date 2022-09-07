ba164957c16573709fe1b8cad82433524238eb45
Tiago de Castro Lima <casmei@protonmail.com>
2022-09-06 21:21:45 -0300

    initial commit

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export class Button extends Component {
+    render() {
+        const {text, quandoClica} = this.props;
+        return (
+            <button onClick={quandoClica}>
+                {text}
+            </button>
+
+        )
+    }
+}
